d3b6780e17c01a7b48a857f8fb332869769e97c2
Edward Jacobs <eddielawrence23@gmail.com>
2019-12-10 18:40:28 +0100

    FEAT: add images to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,8 @@ Artist.destroy_all
 Favorite.destroy_all
 ViewingLocation.destroy_all
 
+# Test
+
 ed = User.create(username: 'Edward', email: 'ed@gmail.com', password: '123456'),
 gijs = User.create(username: 'Gijs', email: 'gijs@gmail.com', password: '123456'),
 ben = User.create(username: 'Ben', email: 'ben@gmail.com', password: '123456'),
@@ -23,6 +25,20 @@ monalisa = Artwork.create(viewing_location: le_louvre, artist: picasso, photo: U
 User.all.each do |user|
   favorite = Favorite.create(user: user, artwork: monalisa)
 end
+
+Artwork.create!(viewing_location: le_louvre, title: 'hello world', artist: picasso, photo: URI("https://images.unsplash.com/photo-1541680670548-88e8cd23c0f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"))
+sleep(3)
+Artwork.create!(viewing_location: le_louvre, title: 'hello world', artist: picasso, photo: URI("https://images.unsplash.com/photo-1494236536165-dab4d859818b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"))
+sleep(3)
+Artwork.create!(viewing_location: le_louvre, title: 'hello world', artist: picasso, photo: URI("https://images.unsplash.com/photo-1501181726133-750ba3cf73c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"))
+sleep(3)
+Artwork.create!(viewing_location: le_louvre, title: 'hello world', artist: picasso, photo: URI("https://images.unsplash.com/photo-1551732998-9573f695fdbb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"))
+sleep(3)
+Artwork.create!(viewing_location: le_louvre, title: 'hello world', artist: picasso, photo: URI("https://images.unsplash.com/photo-1552084117-56a987666449?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"))
+sleep(3)
+Artwork.create!(viewing_location: le_louvre, title: 'hello world', artist: picasso, photo: URI("https://images.unsplash.com/photo-1508341421810-36b8fc06075b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"))
+
+
 
 
 
