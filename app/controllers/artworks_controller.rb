35c0092770321c1ac2134ce7c8da2c4e1b785bb8
Edward Jacobs <eddielawrence23@gmail.com>
2019-12-11 16:24:42 +0100

    FEAT: search by location

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,6 +1,12 @@
 class ArtworksController < ApplicationController
   def index
     @artworks = Artwork.all
+    if params[:search][:location]
+      @location = ViewingLocation.find_by(name: params[:search][:location])
+      @location_id = @location.id if @location
+      @artworks = Artwork.where(viewing_location_id: @location_id)
+    else
+    end
   end
 
   def show
