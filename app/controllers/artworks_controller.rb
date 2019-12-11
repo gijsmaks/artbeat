6c73afb6ea8b31cd6122eb432c6a440a64549cd2
Edward Jacobs <eddielawrence23@gmail.com>
2019-12-11 16:52:20 +0100

    FIX: Wrap search in conditional

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,11 +1,12 @@
 class ArtworksController < ApplicationController
   def index
     @artworks = Artwork.all
-    if params[:search][:location]
-      @location = ViewingLocation.find_by(name: params[:search][:location])
-      @location_id = @location.id if @location
-      @artworks = Artwork.where(viewing_location_id: @location_id)
-    else
+    if params[:search]
+      if params[:search][:location]
+        @location = ViewingLocation.find_by(name: params[:search][:location])
+        @location_id = @location.id if @location
+        @artworks = Artwork.where(viewing_location_id: @location_id)
+      end
     end
   end
 
