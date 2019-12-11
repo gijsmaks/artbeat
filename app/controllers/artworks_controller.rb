512f2f8c99b7eb5d7f4fc1458bdf92b373958eaa
Edward Jacobs <eddielawrence23@gmail.com>
2019-12-11 17:02:51 +0100

    FEAT: add No results found for specific location on index page

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,13 +1,12 @@
 class ArtworksController < ApplicationController
   def index
     @artworks = Artwork.all
-    if params[:search]
-      if params[:search][:location]
-        @location = ViewingLocation.find_by(name: params[:search][:location])
-        @location_id = @location.id if @location
-        @artworks = Artwork.where(viewing_location_id: @location_id)
-      end
-    end
+    return unless params[:search]
+    return unless params[:search][:location]
+
+    @location = ViewingLocation.find_by(name: params[:search][:location])
+    @location_id = @location.id if @location
+    @artworks = Artwork.where(viewing_location_id: @location_id)
   end
 
   def show
