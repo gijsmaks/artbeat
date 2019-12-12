5f60cb3213716aff87ed8dba18381db884ae5a42
Edward Jacobs <eddielawrence23@gmail.com>
2019-12-12 15:07:21 +0100

    FIX: Add dependent destroy to comments from Artwork model

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -5,7 +5,7 @@ class Artwork < ApplicationRecord
   has_many :categorys, through: :artwork_categorys
   has_many :favorites, dependent: :destroy
   has_many :saved_artworks
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   include PgSearch::Model
   pg_search_scope :search_by,
