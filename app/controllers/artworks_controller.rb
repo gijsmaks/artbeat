2225f1bc41a5c8786773f5d60099b2f0ba2f9456
Edward Jacobs <eddielawrence23@gmail.com>
2019-12-12 14:47:56 +0100

    FEAT: add pg search to ArtworksController

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,13 +1,20 @@
 class ArtworksController < ApplicationController
   def index
     @artworks = Artwork.all
-    return unless params[:search]
-    return unless params[:search][:location]
+    # return unless params[:search]
+    # return unless params[:search][:location]
 
-    @location = ViewingLocation.find_by(name: params[:search][:location])
-    @location_id = @location.id if @location
+    # @location = ViewingLocation.find_by(name: params[:search][:location])
+    # @location_id = @location.id if @location
+    # @artworks = Artwork.where(viewing_location_id: @location_id)
+    if params[:search].present?
+      @artworks = Artwork.search_by(params[:search])
+    else
+      @artworks = Artwork.all
+    end
+
     # Artwork.search_by("picasso").first
-    @artworks = Artwork.where(viewing_location_id: @location_id)
+
   end
 
   def show
