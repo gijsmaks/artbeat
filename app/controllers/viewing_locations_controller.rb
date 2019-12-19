fdc63ffb804f29f6dab8764df140c14780ad3631
gijs maks <gijsmaks@gmail.com>
2019-12-19 14:11:03 +0100

    feat: fix

--- app/controllers/viewing_locations_controller.rb
+++ app/controllers/viewing_locations_controller.rb
@@ -13,10 +13,13 @@ class ViewingLocationsController < ApplicationController
   def show
     @viewing_location = ViewingLocation.find(params[:id])
     @artworks = Artwork.where(viewing_location_id: @viewing_location.id)
+
+
     @markers = [{
       lat: @viewing_location.latitude,
       lng: @viewing_location.longitude,
-      infoWindow: render_to_string(partial: "info_window", locals: { viewing_location: viewing_location })
+      infoWindow: render_to_string(partial: "info_window", locals: { viewing_location: @viewing_location }),
+      image_url: helpers.asset_path('artbeat-map-marker.png')
     }]
   end
 end
